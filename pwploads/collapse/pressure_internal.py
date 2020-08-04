763dcbad8af0eb748e982444e5a76191969e3830
JUANG <camiloang94@gmail.com>
2020-08-04 11:53:31 +0200

    internal pressure profiles (collapse cases)

---
+++ pwploads/collapse/pressure_internal.py
@@ -0,0 +1,23 @@
+def inside_full(tvd, rho_fluid, g):
+    p_int = [g * rho_fluid * x for x in tvd]
+
+    return p_int
+
+
+def partial_evacuation(tvd, rho_mud, tvd_zone, p_zone, g):
+    tvd_mud_droplevel = tvd_zone - p_zone / (g * rho_mud)
+
+    p_int = []
+    for x in tvd:
+        if x <= tvd_mud_droplevel:
+            p_int.append(0)
+        else:
+            p_int = g * rho_mud * (x - tvd_mud_droplevel)
+
+    return p_int
+
+
+def full_evacuation(tvd):
+    p_int = [0] * len(tvd)
+
+    return p_int
